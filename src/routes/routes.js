a6c7d813725101415fa807db5496e2bcb04310a5
Jim <jdmedlock@gmail.com>
2024-08-16 19:07:07 -0500

    Refactor: Resolve merge conflicts

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import { handleGitHubEvents } from '../controllers/handleGitHubEvents.js'
-import { handleClickupEvents } from '../controllers/handleClickupEvents.js'
 
 const router = express.Router()
 
