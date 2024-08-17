50e3aa20a547ea17ca80340e7b04a9579ad39437
Jim <jdmedlock@gmail.com>
2024-08-16 19:06:28 -0500

    Refactor: Resolve merge conflicts

--- src/util/shutdown.js
+++ src/util/shutdown.js
@@ -1,22 +1,8 @@
-<<<<<<< HEAD
 
 // Gracefully terminate when the server is shutdown or terminated
 const shutdown = (server, connections) => {
   console.log('Received kill signal, shutting down gracefully')
 
-=======
-import deleteClickupWebhook from './deleteClickupWebhook.js'
-
-// Gracefully terminate when the server is shutdown or terminated
-const shutdown = (server, connections, clickUpWebhookID) => {
-  console.log('Received kill signal, shutting down gracefully')
-
-  if (clickUpWebhookID) {
-    console.log(`Terminating Clickup Webhook: ${ clickUpWebhookID }`)
-    deleteClickupWebhook(clickUpWebhookID)
-  }
-
->>>>>>> ce36b261dd3b2b6ac0f09981c7e72d2fc183d36c
   server.close(() => {
       console.log('Closed out remaining connections')
       process.exit(0)
