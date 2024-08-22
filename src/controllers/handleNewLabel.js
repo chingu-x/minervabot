2c4aba9c0990f6311c9c14698aea8d22310c19f8
jdmedlock <jdmedlock@gmail.com>
2024-08-22 14:42:32 -0500

    Feature: Add logic to update task tag in Clickup [WIP]

--- src/controllers/handleNewLabel.js
+++ src/controllers/handleNewLabel.js
@@ -3,7 +3,7 @@ import { Octokit } from 'octokit'
 
 // When a new label is assigned to the issue add a cooresponding tag to the
 // cooresponding ClickUp Task.
-const handleNewLabel = async (githubIssueNo, issueLabels) => {
+const handleNewLabel = async (githubIssueNo, labelName) => {
   console.log(`handleLabelChanges - githubIssueNo:${ githubIssueNo } issueLabels:`, issueLabels)
 
   try {
@@ -13,7 +13,7 @@ const handleNewLabel = async (githubIssueNo, issueLabels) => {
     
     // Retrieve the associated ClickUp Task ID that was added as a comment to 
     // the GitHub Issue    
-    const issueResponse = await octokit.request('GET /repos/{owner}/{repo}/issues/{issue_number}/comments', {
+    const issueComments = await octokit.request('GET /repos/{owner}/{repo}/issues/{issue_number}/comments', {
       owner: process.env.GITHUB_ORG,
       repo: process.env.GITHUB_REPO,
       issue_number: githubIssueNo,
@@ -23,6 +23,40 @@ const handleNewLabel = async (githubIssueNo, issueLabels) => {
     })
 
     console.log(`handleNewLabel - issueResponse: `, issueResponse)
+    const searchString = '**DO NOT MODIFY/DELETE THIS COMMENT**\nProject task ID: '
+    let taskID
+    for (let comment of issueComments) {
+      const taskIDIndex = comment.body.indexOf(searchString)
+      if (taskIDIndex) {
+        taskID = comment.body.slice(taskIDIndex+searchString.length)
+        console.log(`handleNewLabel - taskID:${ taskID }`)
+        break
+      }
+    }
+
+    // Add the new label as a tag on the associated Clickup Task
+    if (taskID) {
+      const query = new URLSearchParams({
+        custom_task_ids: 'true',
+        team_id: process.env.CLICKUP_TEAM_ID
+      }).toString()
+    
+      const taskId = taskID
+      const tagName = labelName
+      const response = await fetch(
+        `https://api.clickup.com/api/v2/task/${taskId}/tag/${tagName}?${query}`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: process.env.CLICKUP_API_KEY
+          }
+        }
+      )
+    
+      console.log(`handleNewLabel - resp:`, response)
+    }
+
   }
   catch (error) {
     throw Error(`connectIssueToClickup - clickupTaskID:${clickupTaskID} githubIssueNo:${githubIssueNo} error:`, error)
