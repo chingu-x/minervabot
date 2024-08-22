61abdef609cd89c546da6b436e76129d67cdecb8
jdmedlock <jdmedlock@gmail.com>
2024-08-22 15:16:57 -0500

    Feature: Add logic to update task tag in Clickup [WIP]

--- src/controllers/handleNewLabel.js
+++ src/controllers/handleNewLabel.js
@@ -28,36 +28,33 @@ const handleNewLabel = async (githubIssueNo, labelName) => {
     const searchString = '**DO NOT MODIFY/DELETE THIS COMMENT**\nProject task ID: '
     for (let comment of issueComments) {
       const taskIDIndex = comment.body.indexOf(searchString)
-      if (taskIDIndex) {
+      if (taskIDIndex !== -1) {
+        // Add the new label as a tag on the associated Clickup Task
         taskID = comment.body.slice(taskIDIndex+searchString.length)
         console.log(`handleNewLabel - taskID:${ taskID }`)
+
+        const query = new URLSearchParams({
+          custom_task_ids: 'true',
+          team_id: process.env.CLICKUP_TEAM_ID
+        }).toString()
+      
+        const taskId = taskID
+        const tagName = labelName
+        const response = await fetch(
+          `https://api.clickup.com/api/v2/task/${taskId}/tag/${tagName}?${query}`,
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: process.env.CLICKUP_API_KEY
+            }
+          }
+        )
+      
+        console.log(`handleNewLabel - resp:`, response)
         break
       }
     }
-
-    // Add the new label as a tag on the associated Clickup Task
-    if (taskID) {
-      const query = new URLSearchParams({
-        custom_task_ids: 'true',
-        team_id: process.env.CLICKUP_TEAM_ID
-      }).toString()
-    
-      const taskId = taskID
-      const tagName = labelName
-      const response = await fetch(
-        `https://api.clickup.com/api/v2/task/${taskId}/tag/${tagName}?${query}`,
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: process.env.CLICKUP_API_KEY
-          }
-        }
-      )
-    
-      console.log(`handleNewLabel - resp:`, response)
-    }
-
   }
   catch (error) {
     throw Error(`connectIssueToClickup - taskID:${taskID} githubIssueNo:${githubIssueNo} labelName:${labelName} error:`, error)
