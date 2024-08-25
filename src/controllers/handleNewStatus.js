abdd6093320d84e5f016f27fe8a7f4b352f6d907
Jim <jdmedlock@gmail.com>
2024-08-24 20:04:29 -0500

    Feature: Add task status to Clickup Task [WIP]

--- src/controllers/handleNewStatus.js
+++ src/controllers/handleNewStatus.js
@@ -4,47 +4,47 @@ import getTaskID from '../util/getTaskID.js'
 
 // When a new status label (`status/...`) is assigned to the issue add a cooresponding 
 // tag to the cooresponding ClickUp Task.
-const handleNewStatus = async ((githubIssueNo, taskStatus) => {
-    console.log(`handleNewStatus - githubIssueNo:${ githubIssueNo } labelName:`, labelName)
+const handleNewStatus = async (githubIssueNo, taskStatus) => {
+  console.log(`handleNewStatus - githubIssueNo:${ githubIssueNo } taskStatus:`, taskStatus)
 
-    let taskID
-  
-    try {
-      const octokit = new Octokit({
-        auth: process.env.GITHUB_TOKEN
-      })
-      
-      // Retrieve the associated ClickUp Task ID that was added as a comment to 
-      // the GitHub Issue  
-      taskID = await getTaskID(githubIssueNo) 
-      if (taskID !== -1) {
-        // Extract the Clickup status from the new label and add it to the
-        // associated Clickup Task
-        const query = new URLSearchParams({
+  let taskID
+
+  try {
+    const octokit = new Octokit({
+      auth: process.env.GITHUB_TOKEN
+    })
+    
+    // Retrieve the associated ClickUp Task ID that was added as a comment to 
+    // the GitHub Issue  
+    taskID = await getTaskID(githubIssueNo) 
+    if (taskID !== -1) {
+      // Extract the Clickup status from the new label and add it to the
+      // associated Clickup Task
+      const query = new URLSearchParams({
           custom_task_ids: 'true',
           team_id: process.env.CLICKUP_TEAM_ID
-        }).toString()
+      }).toString()
       
-        const taskId = taskID
-        const response = await fetch(
-            `https://api.clickup.com/api/v2/task/${taskId}?${query}`,
-            {
-              method: 'PUT',
-              headers: {
-                'Content-Type': 'application/json',
-                Authorization: process.env.CLICKUP_API_KEY
-              },
-              body: JSON.stringify({
-                status: taskStatus,
-              })
-            }
-          )
-      }
+      const taskId = taskID
+      const response = await fetch(
+        `https://api.clickup.com/api/v2/task/${taskId}?${query}`,
+        {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: process.env.CLICKUP_API_KEY
+          },
+          body: JSON.stringify({
+            status: taskStatus
+          })
+        }
+      )
     }
-    catch (error) {
+  }
+  catch (error) {
       throw Error(`handleNewStatus - taskID:${taskID} githubIssueNo:${githubIssueNo} labelName:${labelName} error:`, error)
-    }
-    return
+  }
+  return
 }
 
 export default handleNewStatus
