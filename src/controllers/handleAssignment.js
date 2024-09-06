ee6fe85d1351706f4736bbc9b9b557ce63de80ac
Jim <jdmedlock@gmail.com>
2024-09-05 19:16:11 -0500

    Feature: Add logic to add/remove assignees [WIP]

--- src/controllers/handleAssignment.js
+++ src/controllers/handleAssignment.js
@@ -1,32 +1,11 @@
 import getClickupUserID from '../util/getClickupUserID.js'
 import getTaskID from '../util/getTaskID.js'
+import userTranslationMap from '../util/userTranslationMap.js'
 
 // When a new status label (`status/...`) is assigned to the issue add a cooresponding 
 // tag to the cooresponding ClickUp Task.
-const handleAssignment = async (githubIssueNo, body) => {
-  console.log(`handleAssignment - githubIssueNo:${ githubIssueNo } body:`, body)
-
-  const userNameTranslationMap = [
-    { githubUserName: 'Ajen07', clickupUserName: 'Arman Kumar Jena' },
-    { githubUserName: '', clickupUserName: 'Austin Tang' },
-    { githubUserName: 'siasktv', clickupUserName: 'Bianca Benitez' },
-    { githubUserName: 'cherylii', clickupUserName: 'Cheryl' },
-    { githubUserName: 'curlwl', clickupUserName: 'Curt W' },
-    { githubUserName: 'Dan-Y-Ko', clickupUserName: 'Dan' },
-    { githubUserName: 'eury-k', clickupUserName: 'Eury Kim' },
-    { githubUserName: '', clickupUserName: 'J. R.' },
-    { githubUserName: 'JaneMoroz', clickupUserName: 'Jane Moroz' },
-    { githubUserName: 'jdmedlock', clickupUserName: 'Jim Medlock' },
-    { githubUserName: 'joekotvas', clickupUserName: 'Joseph Kotvas' },
-    { githubUserName: 'JoshuaHinman', clickupUserName: 'Josh Hinman' },
-    { githubUserName: 'MattRueter', clickupUserName: 'Matt Rueter' },
-    { githubUserName: 'mladen-krstin', clickupUserName: 'Mladen Krstin' },
-    { githubUserName: 'roziii', clickupUserName: 'Rozi' },
-    { githubUserName: 'ellrub', clickupUserName: 'Ruben Ellefsen' },
-    { githubUserName: 'timDeHof', clickupUserName: 'Tim DeHof' },
-    { githubUserName: 'timothyrusso', clickupUserName: 'Timothy Russo' },
-    { githubUserName: 'winniecwng', clickupUserName: 'Winnie Ng' },
-  ]
+const handleAssignment = async (action, githubIssueNo, body) => {
+  console.log(`handleAssignment - action:${ action } githubIssueNo:${ githubIssueNo } body:`, body)
 
   let taskID
 
@@ -36,10 +15,11 @@ const handleAssignment = async (githubIssueNo, body) => {
     taskID = await getTaskID(githubIssueNo) 
     if (taskID !== -1) {
       // Lookup the Clickup user ID for the GitHub user assigned to the task
-      const userNameTranslation = userNameTranslationMap.find(( entry ) => entry.githubUserName === body.issue.assignee.login)
-      console.log(`handleAssignment - userNameTranslation: `, userNameTranslation)
-      if (userNameTranslation !== undefined) {
-        const clickupUserID = await getClickupUserID(userNameTranslation.clickupUserName)
+      const userName = userTranslationMap.find(( entry ) => entry.githubUserName === body.issue.assignee.login)
+      console.log(`handleAssignment - userName: `, userName)
+      if (userName !== undefined) {
+        const clickupUserID = await getClickupUserID(userTranslation.clickupUserName)
+        const assigneeAction = action === 'assigned' ? '{add: [clickupUserID]}' : '{rem: [clickupUserID]}'
         const query = new URLSearchParams({
           custom_task_ids: 'true',
           team_id: process.env.CLICKUP_TEAM_ID
@@ -54,7 +34,7 @@ const handleAssignment = async (githubIssueNo, body) => {
               Authorization: process.env.CLICKUP_API_KEY
             },
             body: JSON.stringify({
-              assignees: { add: [clickupUserID], rem: [] },
+              assignees: assigneeAction,
             })
           }
         )
